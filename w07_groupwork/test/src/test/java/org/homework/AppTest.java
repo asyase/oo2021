0d6c946304544c73a9e2136bb8a08bbd9a923927
Anastasija Selevjorstova <selevjorstovaa@gmail.com>
2021-04-16 17:43:31 +0300

    Tund7

--- w07_groupwork/test/src/test/java/org/homework/AppTest.java
+++ w07_groupwork/test/src/test/java/org/homework/AppTest.java
@@ -1,6 +1,4 @@
-
 package org.homework;
-
 
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
@@ -8,44 +6,38 @@ import org.junit.BeforeClass;
 
 import org.junit.Test;
 
-
-
-
-public class AppTest 
-{
+public class AppTest {
     static App game;
 
     @BeforeClass
-    public static void init(){
-         game = new App();
+    public static void init() {
+        game = new App();
     }
 
     @Test
-    public void checkGameBorders()
-    {
+    public void checkGameBorders() {
         assertTrue(game.width == 35);
         assertTrue(game.height == 10);
     }
 
     @Test
-    public void scoreStartsWithZero(){
+    public void scoreStartsWithZero() {
         assertTrue(game.score == 0);
     }
 
     @Test
-    public void isLocationShown(){
+    public void isLocationShown() {
         assertTrue(game.toString() != "");
     }
 
-
     @Test(timeout = 15000)
-    public void isTestResultFalse(){
+    public void isTestResultFalse() {
         assertFalse(game.testResult() == "Not Ready");
     }
 
     @Test
-    public void isPlayerInBorder(){
-        
+    public void isPlayerInBorder() {
+
     }
 
 }
